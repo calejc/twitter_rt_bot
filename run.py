df90004c8a01ed540f5c170744219cb35c0903eb
Cale Cortney <thortney@mailbox.org>
2021-02-11 08:16:45 -0600

    fixed bug where stream listener was RTing replies from undesired users. now only RTs tweets that are tweeted by users that we follow

--- run.py
+++ run.py
@@ -10,11 +10,12 @@ load_dotenv()
 logging.basicConfig(filename='bot.log', filemode='w', level=logging.WARNING)
 
 class TweepyStreamListener(tweepy.StreamListener):
-    def __init__(self, api):
+    def __init__(self, api, users):
         self.api = api
+        self.users = users
 
     def on_status(self, status):
-        if not status.retweeted:
+        if status._json['user']['id_str'] in self.users:
             try:
                 self.api.retweet(status.id)
             except Exception as e:
@@ -32,15 +33,16 @@ def return_api():
     auth.set_access_token(os.getenv('ACCESS_TOKEN'), os.getenv('ACCESS_SECRET'))
     return tweepy.API(auth)
 
-def follow(users):
+def stringify(users):
     # convert user IDs to strings
     # stream filter 'follow' takes array of string Ids, not integers.
     return [str(userId) for userId in users]
 
 def run(api):
-    streamListener = TweepyStreamListener(api)
+    users = stringify(api.friends_ids())
+    streamListener = TweepyStreamListener(api, users)
     stream = tweepy.Stream(auth = api.auth, listener=streamListener)
-    stream.filter(follow=follow(api.friends_ids()))
+    stream.filter(follow=stringify(api.friends_ids()))
 
 
 if __name__ == "__main__":
